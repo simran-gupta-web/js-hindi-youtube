b092e47b4c843e0d7d55c1ec439aa5e86fdf373b
Simran <simrangupta210201@gmail.com>
2025-08-17 18:47:30 +0530

    done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 14553
+let accountEmail = "simran@google.com"
+var accountPasswordc= "12233"
+accountCity ="Jaipur"
+let accountState ;
+
+//accountId = 2 // mot allowed 
+
+
+ accountEmail= "hc@hc.com"
+ accountPasswordc = "3423343"
+ accountCity = "Bengaluru"
+
+ console.log(accountId);
+ /* prefer not to use var 
+    beacuase of issue in block scope and functional scope
+*/
+ console.table([accountId, accountEmail, accountPasswordc, accountCity, accountState])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+ 
+//alert() // we are usin nodejs not browser
+
+console.log(3+3);
+console.log("Simran")
+
+let name = "simran"
+let age = 18
+let isLoggedin = false
+let state;
+
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+//object 
+
+console.log(typeof "simran");
+console.log(typeof age);
+console.log(typeof null);
