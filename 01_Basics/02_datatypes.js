ba83c9b7335b2540800f08bdffb1b210d3cd3fdd
Simran <simrangupta210201@gmail.com>
2025-08-19 19:13:32 +0530

    done

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -23,3 +23,56 @@ let state;
 console.log(typeof "simran");
 console.log(typeof age);
 console.log(typeof null);
+
+// Primitive datatypes
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+ 
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id===anotherid);
+
+//Reference type (non primitive)
+// type : array, objects, functions 
+
+const heros = ["shaktiman","naagraj", "doga"]
+{
+name:"simran"
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof anotherid);
+
+
+//+++++++++++++++++++++++++++++++++
+//Memory
+//Stack (Primitive), Heap(Non primitive)
+
+let myYoutubename  = "simranguptadotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userone = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let usertwo = userone
+
+usertwo.email = "simran@google.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,15 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2!=1);
+
+//console.log("2">1);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0); //avoid these type of comparison
+
+//=== 
+console.log("2"===2);
