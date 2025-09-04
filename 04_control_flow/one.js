9f4794804da189dfdba210e83fd029c6ab0fd856
Simran <simrangupta210201@gmail.com>
2025-09-04 22:52:47 +0530

    done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,61 @@
+//if 
+/*const isUserloggedIn = true
+const temperature = 41
+
+if (temperature === 40 ){
+console.log("less than 50");
+}
+else{
+    console.log("temperature is greater than 50");
+}
+ */
+
+//< , >, <=, >=, == , !=, ===, !==
+
+/*const score = 200
+
+if(score> 100){
+    let power = "fly"
+    console.log(`User power: ${power}`);
+}
+
+console.log(`User power: ${power}`);
+*/
+
+
+/*
+ const balance = 1000
+
+if(balance < 500) {
+console.log("less than");
+}else if (balance < 750) {
+    console.log("less than 750");
+}
+
+else if (balance < 900) {
+    console.log("less than 900");
+}
+
+else  {
+    console.log("less than 1200");
+}
+
+*/
+
+const userLoggedIn = true 
+const debitCard = true
+const loggedInFromgoogle = false
+const loggedInFromemail = true 
+
+if ( userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+    
+}
+ if (loggedInFromgoogle || loggedInFromemail) {
+    console.log("user logged in ");
+    
+ }
+
+
+ 
+
